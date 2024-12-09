e492f9f6ba25beefd48d1a9ef1eb1f64eaf71a90
kun98-liu <liujiankun0721@gmail.com>
2024-12-09 18:38:36 +0900

    Add log support

--- api-server/app/main.py
+++ api-server/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from starlette.exceptions import HTTPException
+
 from app.routers import student, login, user
+from app.server.api_logger_middleware import APILoggerMiddleware
+from app.server.http_exception_handler import http_exception_handler
 
 app = FastAPI(
     docs_url="/swagger",
@@ -9,3 +13,8 @@ app = FastAPI(
 app.include_router(student.router)
 app.include_router(login.router)
 app.include_router(user.router)
+
+app.add_middleware(APILoggerMiddleware)
+
+app.add_exception_handler(HTTPException, http_exception_handler)
+
