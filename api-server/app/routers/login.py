8c3af666fef698a0d11a6f4a68251dc4389401aa
kun98-liu <81949316+kun98-liu@users.noreply.github.com>
2024-12-10 12:01:03 +0900

    Add log config and custom API logger to log requests (#6)
    uvicorn log access of API like
    
    ```sh
    api-server-1  | INFO:     192.168.65.1:41988 - "GET /swagger HTTP/1.1" 200 OK
    ```
    
    but it's not helping, because it does not log the request body.
    
    - Following this issue https://github.com/encode/uvicorn/issues/491, I
    made a custom log config to add logger name in log and adjust log format
    - Add a `middleware` of fastapi to log the request in a more detailed
    way.
    - Override HTTPExceptionHandler to create error log when API is not
    responding 200.
    https://fastapi.tiangolo.com/tutorial/handling-errors/#override-the-httpexception-error-handler
    - Hide uvicorn access log 
    
    Now the log looks like:
    ```sh
    api-server-1  | 2024-12-09 09:35:13,363 - INFO     - app.api_logger       - [request_id: 7608958e-36a6-428e-8427-83ced6b9d7c3, method:POST, path: http://0.0.0.0:8000/login/] Request body: {"e_mail":"123","password":"string"}.
    api-server-1  | 2024-12-09 09:35:13,367 - INFO     - app.routers.login    - e_mail='123' password='string'
    api-server-1  | 2024-12-09 09:35:13,367 - ERROR    - app.http_exc_handler - [request_id: 7608958e-36a6-428e-8427-83ced6b9d7c3, method:POST, path: http://0.0.0.0:8000/login/] Failed to handle request. status_code=404, detail=asdasdas
    ```
    
    
    **What can be improved in the future:**
    - Instead of only outputting logs to console, we should also keep logs
    as files in disk and use logstash to send logs to es like real-world
    application does.
    - Response is not logged, but we may want to add it. APILoggerMiddleware
    has to build a dummy Response from the outgoing bytestreams, which is
    not good. So we may want to find another way to do so.

--- api-server/app/routers/login.py
+++ api-server/app/routers/login.py
@@ -1,16 +1,18 @@
+import logging
 from fastapi import APIRouter
-
 from app.schema.loginschema import LoginRequestSchema, LoginResponseSchema, SigninResponseSchema, SigninRequestSchema
 
 router = APIRouter(prefix="/login", tags=["login"])
+logger = logging.getLogger(__name__)
 
 
 @router.post("/", response_model=LoginResponseSchema)
 async def login(login_request: LoginRequestSchema):
-    print(f"{login_request}")
+    logger.info(f"{login_request}")
     return LoginResponseSchema(token="nidetoken", user_id="niyeye")
+
 
 @router.post("/signin", response_model=SigninResponseSchema)
 async def signin(signin_request: SigninRequestSchema):
-    print(f"{signin_request}")
+    logger.info(f"{signin_request}")
     return SigninResponseSchema(success=True)
